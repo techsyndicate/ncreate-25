0281aefcb122e43d19f9748f2d6f59d8440b39b0
bhavitgrover <bhavit.grover@outlook.com>
2025-10-16 13:42:45 +0530

    added things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,115 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div>
+      <div style={{ position: "fixed", top: "6vw", left: "7vw" }}>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          alt=""
+          width={50}
+          height={50}
+          src="/backBtn.png"
+          style={{ height: "5vw", width: "auto" }}
+        ></Image>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "6vw",
+          gap: "2vw",
+        }}
+      >
+        <Image
+          alt=""
+          width={50}
+          height={50}
+          src="/profileIcon.png"
+          style={{ width: "6vw" }}
+        ></Image>
+        <p style={{ fontSize: "4vw", lineHeight: "1" }}>J0xn_d03</p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "5vw",
+          marginTop: "6.7vh",
+        }}
+      >
+        <Image
+          alt=""
+          width={50}
+          height={50}
+          src="/leftButton.png"
+          style={{ width: "6vw" }}
+        ></Image>
+        <div>
+          <Image
+            alt=""
+            width={200}
+            height={400}
+            src="/bhavit-grover.png"
+            style={{ width: "67vw" }}
+          ></Image>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Image
+          alt=""
+          width={50}
+          height={50}
+          src="/rightButton.png"
+          style={{ width: "6vw" }}
+        ></Image>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          width: "60vw",
+          height: "12vw",
+          marginLeft: "20vw",
+          display: "flex",
+          alignItems: "center",
+          marginTop: "1.5vh",
+        }}
+      >
+        <Image
+          src="/dottedLine.png"
+          width={200}
+          height={10}
+          alt=""
+          style={{ width: "60vw" }}
+        ></Image>
+        <Image
+          src="/slider.png"
+          width={50}
+          height={50}
+          alt=""
+          style={{
+            height: "12vw",
+            width: "auto",
+            position: "absolute",
+            left: "24vw",
+          }}
+        ></Image>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          height: "25vh",
+          bottom: "0",
+          width: "104vw",
+          left: "-2vw",
+        }}
+      >
+        <Image
+          alt=""
+          width={300}
+          height={200}
+          src="/tedhiMedhiPhotu.png"
+          style={{ width: "104vw", height: "auto", position: "absolute", bottom: "0" }}
+        ></Image>
+      </div>
     </div>
   );
 }
